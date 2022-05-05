b6e1b4fe6ed0b8a377dd416a983b551f857a3d13
kommusravani <104352484+kommusravani@users.noreply.github.com>
2022-05-05 12:43:01 +0530

    Add files via upload

---
+++ naming variables.py
@@ -0,0 +1,31 @@
+a=2
+b=5
+print(a,b)
+print(a)
+print(b)
+a=4;b=2
+print(a)
+print(b)
+a=2;b=5;c=10;print(a);print(b);print(c)
+x=5;y=2;z=15;print(x,y,z)
+a=10;b=15;c=d=20;print(a,b,c,d)
+a=5;b=2;print(a),print(b)
+a=5
+b=9
+print(a+b)
+print(a*b)
+print(a-b)
+print(a/b)
+print()
+print()
+print()
+print(a)
+
+a="python"
+print(len(a))
+b=" python "
+print(len(b))
+a="python"
+print(a[2])
+b="   python   "
+print(b[1])
